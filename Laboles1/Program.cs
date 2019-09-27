c95ddfac397c6feace0b75c4d87dac118a5fb20e
Guillaume de Oliveira <guillaume2000@hotmail.be>
2019-09-27 09:23:57 +0200

    Eerste versie, hardcoded datum

--- Laboles1/Program.cs
+++ Laboles1/Program.cs
@@ -10,6 +10,17 @@ namespace Laboles1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wil je de datum van vandaag?");
+            string Antw = Console.ReadLine().ToLower();
+
+            if (Antw == "ja")
+            {
+                Console.WriteLine("De datum is 27/09/2019");
+            }
+            else
+            {
+                Console.WriteLine("Oke sorry eh beast");
+            }
         }
     }
 }
